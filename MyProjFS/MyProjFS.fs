8ed806f21f80da95da9a71ca2f21386106425479
Олег Высоцкий <olegbom@gmail.com>
2017-03-22 19:08:05 +0300

    Пищим базером

--- MyProjFS/MyProjFS.fs
+++ MyProjFS/MyProjFS.fs
@@ -1,6 +1,20 @@
 module MyProjFS
+open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World!!SD" 
+    printfn "Hello World!!SD"
+    let la = 440
+
+    let getMeTone index = 
+        let la = 440 
+        int (float la * Math.Pow(2.0,float index/12.0))
+
+    Console.Beep(getMeTone 1, 200)
+    Console.Beep(getMeTone 2, 200)
+    Console.Beep(getMeTone 3, 200)
+    Console.Beep(getMeTone 4, 200)
+    Console.Beep(getMeTone 5, 200)
+    Console.Beep(getMeTone 6, 200)
+    Console.Beep(getMeTone 7, 200)
     0 // return an integer exit code
