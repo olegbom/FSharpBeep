a8802d6ae3153134c1e60946e9d593a7b0fa6ccd
= <=>
2017-03-23 18:10:18 +0300

    vim help file, изменена мелодия

--- MyProjFS/MyProjFS.fs
+++ MyProjFS/MyProjFS.fs
@@ -4,17 +4,17 @@ open System
 [<EntryPoint>]
 let main argv =
     printfn "Hello World!!SD"
-    let la = 440
+    
 
     let getMeTone index = 
         let la = 440 
         int (float la * Math.Pow(2.0,float index/12.0))
 
-    Console.Beep(getMeTone 1, 200)
+    Console.Beep(getMeTone 0, 200)
     Console.Beep(getMeTone 2, 200)
-    Console.Beep(getMeTone 3, 200)
-    Console.Beep(getMeTone 4, 200)
-    Console.Beep(getMeTone 5, 200)
-    Console.Beep(getMeTone 6, 200)
-    Console.Beep(getMeTone 7, 200)
+    Console.Beep(getMeTone 2, 200)
+    Console.Beep(getMeTone 0, 200)
+    Console.Beep(getMeTone 2, 200)
+    Console.Beep(getMeTone 2, 200)
+    Console.Beep(getMeTone 0, 200)
     0 // return an integer exit code
